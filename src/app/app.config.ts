e8a6b478f40fc00f1cfa1bd94f5b1d8e75a5f88a
Vinicius <vinicius.padua@tarvos.ag>
2024-11-23 14:41:08 -0300

    feat: config angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,32 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr(),
+    provideHttpClient(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'timesheet-2b740',
+        appId: '1:841860863724:web:6d27ab6b29b2b4b6bb77c9',
+        storageBucket: 'timesheet-2b740.firebasestorage.app',
+        apiKey: 'AIzaSyBLjQ4-yqnBnjp-nkufvejCBHg6GJnI5aE',
+        authDomain: 'timesheet-2b740.firebaseapp.com',
+        messagingSenderId: '841860863724',
+        measurementId: 'G-Q338MK761R',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
